50d710c133c73e2e6d73eca7f17b9f1bc2c454a7
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-02-17 15:21:46 -0500

    Added semi-functional mouseover function. Periodically changes color of divs within gridContainer. Other times, changes color of entire gridContainer. Needs fix.

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ function generateGrid() {
 
     for(i=0; i < blockAmount; i++) {
         const grid = document.createElement("div");
-        grid.className = "gridBox";
-        document.getElementById("gridContainer").appendChild(grid);
-
-        
+        grid.classList.add('grid');
+        document.getElementById("gridContainer").appendChild(grid); 
     }
 }
 generateGrid(blockAmount);
 
-//Function for hover color change 
+// Function for hover color change 
 const hover = document.getElementById("gridContainer");
 
-hover.addEventListener("mouseenter", function (event) {
-    event.target.style.background = "orange";
+hover.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "orange";
     });
 
+// document.getElementById("gridContainer").onmouseover = function() {hoverColor()};
+
+// function hoverColor() {
+//     document.getElementById("gridContainer").style.backgroundColor = "orange";
+    
+// }
 
 //Function for button to prompt user to input size of gridContainer 
 const gridClear = document.getElementById("gridClear");
