50b43c3affad717bb4c48c789fbab69832e5714f
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-02-18 18:03:09 -0500

    Fixed grid to properly display trail of color on mouseover. Added button to prompt user for input for grid amount and then clear gridContainer.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //Function to create 16 divs using a for loop
-const blockAmount = 256;
-
+const blockAmount = 257;
 function generateGrid() {
-
     for(i=0; i < blockAmount; i++) {
         const grid = document.createElement("div");
         grid.classList.add('grid');
@@ -14,16 +12,32 @@ generateGrid(blockAmount);
 // Function for hover color change 
 const hover = document.getElementById("gridContainer");
 
+
 hover.addEventListener("mouseover", function (event) {
+    if (event.target.id !== "gridContainer") {
     event.target.style.backgroundColor = "orange";
-    });
+    }
+}, );
 
-// document.getElementById("gridContainer").onmouseover = function() {hoverColor()};
+// Function for button to clear grid 
+const gridButton = document.getElementById("gridButton");
 
-// function hoverColor() {
-//     document.getElementById("gridContainer").style.backgroundColor = "orange";
+gridButton.onclick = function() {
+    let gridAmount = prompt("How many squares per side would you like on your new grid?");
+    let element = document.getElementsByClassName('grid');
+    let newGridContainer = document.getElementById('gridContainer');
+    // for loop to clear gridContainer
+    for (i=0; i < blockAmount; i++) {
+        element[i].style.backgroundColor = '';
+    }
+    // for loop to create prompted grid input amount 
     
-// }
+
+    
+    
+}
 
 //Function for button to prompt user to input size of gridContainer 
-const gridClear = document.getElementById("gridClear");
+// gridButton.onclick = function() {
+//     prompt("How many squares per side would you like on your new grid?");
+// }
