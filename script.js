3d18fb40d980b64e6e58f85821b58f436f4d2131
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-02-21 15:13:26 -0500

    Added generateNewGrid function to create new grid using input from prompt box. Added function to remove previous children divs upon mouseclick of prompt box.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //Function to create 16 divs using a for loop
-const blockAmount = 257;
-function generateGrid() {
-    for(i=0; i < blockAmount; i++) {
-        const grid = document.createElement("div");
-        grid.classList.add('grid');
-        document.getElementById("gridContainer").appendChild(grid); 
+const container = document.getElementById("gridContainer");
+function generateGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for(i=0; i < (rows * cols); i++) {
+        const cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell).className = "grid-item";
     }
 }
-generateGrid(blockAmount);
+generateGrid(16,16);
 
 // Function for hover color change 
 const hover = document.getElementById("gridContainer");
-
-
 hover.addEventListener("mouseover", function (event) {
     if (event.target.id !== "gridContainer") {
     event.target.style.backgroundColor = "orange";
@@ -22,34 +22,38 @@ hover.addEventListener("mouseover", function (event) {
 // Function for button to clear grid 
 const gridButton = document.getElementById("gridButton");
 
-gridButton.onclick = function() {
+gridButton.onclick = function () {
     let newGridAmount = prompt("How many squares per side would you like on your new grid?");
-    let element = document.getElementsByClassName('grid');
+    let element = document.getElementsByClassName('grid-item');
+    newGridAmount = parseInt(newGridAmount);
     // for loop to clear gridContainer
-    for (i=0; i < blockAmount; i++) {
+    for (i=0; i < (element.length); i++) {
         element[i].style.backgroundColor = '';
     }
-    if (newGridAmount > 99) {
-        return alert("Please enter fewer than 100 squares per side.")
+    
+    //for loop to create new grid
+    if (newGridAmount < 99) {
+        // delete previous divs 
+        function removeAllChildNodes(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+        }
+        removeAllChildNodes(container);
+        
+    //create new grid
+    function generateNewGrid(newGridAmount) {
+        container.style.setProperty('--grid-rows', newGridAmount);
+        container.style.setProperty('--grid-cols', newGridAmount);
+        for(j=0; j < (newGridAmount * newGridAmount); j++) {
+            const cell = document.createElement("div");
+            cell.innerText = (j + 1);
+            container.appendChild(cell).className = "grid-item";
+        }
     }
-
-    // for loop to create prompted grid input amount 
-    // function generateNewGrid () {
-    //     if (gridAmount > 99) {
-    //         prompt("Please enter a grid of 99 squares or fewer."); 
-    //     } else if (gridAmount < 99) {
-    //         newGridContainer = document.getElementById('gridContainer');
-    //         newGridContainer[0].style.gridTemplateColumns = repeat(gridAmount);
-    //         newGridContainer[0].style.gridTemplateRows = repeat(gridAmount);
-    //     }
-    //     generateGrid(newGridAmount)
-    // }
-
-    
-    
+    generateNewGrid(newGridAmount);
+    // error alert if grid amount is over 99
+    } else if (newGridAmount > 99) {
+        alert("Oops! You broke the machine! Please enter a value lower than 100.")
+    }
 }
-
-//Function for button to prompt user to input size of gridContainer 
-// gridButton.onclick = function() {
-//     prompt("How many squares per side would you like on your new grid?");
-
